47080a5755788cae95c22d990f0e27ba514bc85f
Allyson Stoll <allyson.m.stoll@gmail.com>
2021-11-18 12:36:26 -0800

    Main added to demo.R

--- demo.R
+++ demo.R
@@ -2,7 +2,7 @@
 # date: 2020-01-15
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
@@ -13,5 +13,11 @@ Options:
 
 library(docopt)
 opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
+
+main <- function(opt) {
+  print(opt$arg4)
+  print(opt)
+  print(typeof(opt))
+}
+
+main(opt = opt)
